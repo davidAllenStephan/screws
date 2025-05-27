ecca3c73da73380952297e1d2ea68a2d9fe000d8
davidAllenStephan <david.marino03@hotmail.com>
2025-05-27 10:49:51 -0500

    added more forgot to checkout

---
+++ src/main/java/davidmarino/utility/StopWatch.java
@@ -0,0 +1,21 @@
+/**
+ * Authors: David Allen Stephan Marino
+ * Date: 5/27/25
+ */
+
+package davidmarino.utility;
+
+public class StopWatch {
+    private long startTime;
+    private long stopTime;
+    public StopWatch() {};
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        stopTime = System.currentTimeMillis();
+    }
+    public double seconds() {
+        return (stopTime - startTime) / 1000.00;
+    }
+}
